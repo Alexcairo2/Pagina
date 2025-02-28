a8eecea21830c55693c34afedde16941776eb90a
Alexcairo2 <alexcairogam@gmail.com>
2025-02-28 16:41:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function agregarComentarioDOM(comentarioObj) {
     comentarioDiv.setAttribute("data-id", comentarioObj.id);
 
     comentarioDiv.innerHTML = `
-        <img src="file://C:/Users/alexi/Documents/pima/Imagenes/usuario.png" class="avatar" alt="Avatar">
+        <img src="https://github.com/Alexcairo2/Pagina/blob/paginatest/Imagenes/usuario.png?raw=true" alt="Avatar">
         <div class="comentario-texto">
             <strong>${comentarioObj.nombre}:</strong> <br> ${comentarioObj.comentario}
         </div>
